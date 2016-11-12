18c6eb6b34bf9c0c8248561358d226e9dc3290eb
christian <christian@EquipoChristian>
2016-11-12 16:12:02 +0000

    Comparación de eficiencia del método intercambio por parejas y fusion natural. Version 2. Con fallos

--- src/comparación/Ordenar2Vector.java
+++ src/comparación/Ordenar2Vector.java
@@ -11,20 +11,11 @@ public class Ordenar2Vector implements OrdenarVector {
     
     @Override
     public void ordena(int[] v, DatosEstadisticos de){
-        boolean cambio;
-        int lder, otral, lizq, lsal, movlizq, movlsal, aux;
-        int[] auxvec;
         double t_inicio = currentTimeMillis();
-        
+        boolean cambio = true;
+        int lder, otral, lizq, lsal, movlizq, movlsal, aux;
+        int[] auxvec = new int[v.length-1];
         do {
-            for(int x=0;x<v.length;x++) {
-                System.out.printf(v[x] + ",");
-            }
-            System.out.printf("\n");
-            for(int y=0;y<v.length;y++) {
-                System.out.printf(v[y] + ",");
-            }
-            System.out.printf("\n");
             cambio = false;
             lder = v.length-1; 
             otral = v.length-1;
@@ -32,8 +23,8 @@ public class Ordenar2Vector implements OrdenarVector {
             lsal = 0;
             movlizq = 1;
             movlsal = 1;
-            auxvec = new int[v.length];
-            while (lizq < lder) {
+            
+            while (lizq != lder) {
                 if(v[lizq] > v[lder]) {
                     aux = lizq;
                     lizq = lder;
@@ -69,16 +60,15 @@ public class Ordenar2Vector implements OrdenarVector {
             }
             
         } while(cambio != false);
-        
-        System.out.printf("\nFinal: ");
-        for(int y=0;y<v.length;y++) {
-            System.out.printf(v[y] + ",");
-        }
-        System.out.printf("\n");
-        
         double t_fin = currentTimeMillis();
         de.estableceTiempo((t_fin - t_inicio) / 1000);
-        
-    }    
-    
+        return;
+    } 
+    @Override
+    public void imprimeVector(int[] v){
+        for(int x=0;x<v.length;x++) {
+            System.out.printf(v[x] + ",");
+        }
+        System.out.printf("\n");
+    }  
 }
